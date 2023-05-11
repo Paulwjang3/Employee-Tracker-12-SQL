6e8202c7953d697d2214beda5055dbe8b2430865
Paulwjang3 <128998742+Paulwjang3@users.noreply.github.com>
2023-05-10 22:31:25 -0400

    completed the prompts

--- db/schema.sql
+++ db/schema.sql
@@ -18,11 +18,11 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    employee_id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT NULL,
     FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(employee_id)
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
-const cTable = require('console.table');
 require('dotenv').config();
 
 const db = mysql.createConnection({
     host: 'localhost',
-    port: process.env.PORT || 3001,
     user: 'root',
     password: process.env.PASSWORD,
-    database: 'employee_db'
+    database: 'employee_db',
+    port: 3306,
 });
 
-db.connect(function(err) {
-    if (err) throw err
-    console.log("Connected as Id" + Connection.threadID)
+db.connect(function (err) {
+    if (err) throw err;
     startPrompt();
 });
 
@@ -32,7 +30,7 @@ function startPrompt() {
             viewAllEmployees();
             break;
         case 'Add Employee':
-            addEmployees();
+            addEmployee();
             break;
         case 'Update Employee Role':
             updateEmployeeRole();
@@ -59,18 +57,152 @@ function startPrompt() {
 }
 
 viewAllEmployees = () => {
-    const sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT (manager.first_name, " ", manager.last_name) AS manager FROM employee 
+    const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT (manager.first_name, " ", manager.last_name) AS manager FROM employee 
     LEFT JOIN role ON employee.role_id = role.id
     LEFT JOIN department ON role.department_id = department.id
     LEFT JOIN employee manager ON employee.manager_id = manager.id`;
     
-    db.promise().query(sql, (err, rows) => {
+    db.query(query, (err, res) => {
     if (err) throw err;
-    console.table(rows);
+    console.table(res);
     startPrompt();
-    })
+    });
 };
 
-addEmployee = () => {
-    
+function addEmployee() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: "Enter the employee's first name",
+            name: "firstName"
+        },
+        {
+            type: 'input',
+            message: "Enter the employee's last name",
+            name: 'lastName'
+        },
+        {
+            type: 'input',
+            message: "Enter the employee's role id",
+            name: 'addEmployeeRole'
+        },
+        {
+            type: 'input',
+            message: "Enter the employee's manager id",
+            name: 'addEmployeeManager'
+        }
+    ])
+    .then(function (res) {
+        const firstName = res.firstName;
+        const lastName = res.lastName;
+        const employeeRoleID = res.addEmployeeRole;
+        const employeeManagerID = res.addEmployeeManager;
+        const query = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${firstName}", "${lastName}", "${employeeRoleID}", "${employeeManagerID}")`;
+        db.query(query, function (err, res) {
+            if (err) {
+                throw err;
+            }
+            console.table(res);
+            startPrompt();
+        });
+    });
+}
+
+function updateEmployeeRole() {
+    inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter the employee's ID you want to be updated",
+        name: "updateEmploy"
+      },
+      {
+        type: "input",
+        message: "Enter the new role ID for that employee",
+        name: "newRole"
+      }
+    ])
+    .then(function (res) {
+        const updateEmploy = res.updateEmploy;
+        const newRole = res.newRole;
+        const queryUpdate = `UPDATE employee SET role_id = "${newRole}" WHERE id = "${updateEmploy}"`;
+        db.query(queryUpdate, function (err, res) {
+          if (err) {
+            throw err;
+          }
+          console.table(res);
+          startPrompt();
+        })
+      });
+}
+
+function viewAllRoles() {
+    var query = `SELECT * FROM role`
+    db.query(query, function (err, res) {
+        console.table(res);
+        startPrompt();
+    });
+}
+
+function addRole() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "Enter the employee's title",
+          name: "roleTitle"
+        },
+        {
+          type: "input",
+          message: "Enter the employee's salary",
+          name: "roleSalary"
+        },
+        {
+          type: "input",
+          message: "Enter the employee's department ID",
+          name: "roleDept"
+        }
+      ])
+      .then(function (res) {
+        const title = res.roleTitle;
+        const salary = res.roleSalary;
+        const departmentID = res.roleDept;
+        const query = `INSERT INTO role (title, salary, department_id) VALUES ("${title}", "${salary}", "${departmentID}")`;
+        db.query(query, function (err, res) {
+          if (err) {
+            throw err;
+          }
+          console.table(res);
+          startPrompt();
+        });
+      });
+  }
+  
+function viewAllDepartments() {
+    var query = `SELECT * FROM department`
+    db.query(query, function (err,res) {
+        console.table(res);
+        startPrompt();
+    });
+}
+
+function addDept() {
+    inquirer
+    .prompt({
+        type: 'input',
+        message: 'Enter the name of the new department',
+        name: 'newDept'
+    })
+    .then(function (res) {
+        const newDepartment = res.newDept;
+        const query = `INSERT INTO department (name) VALUES ("${newDepartment}")`;
+        db.query(query, function (err, res) {
+            if (err) {
+                throw err;
+            }
+            console.tables(res);
+            startPrompt();
+        });
+    });
 }
